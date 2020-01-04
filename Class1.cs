518e10ec8b99b4e898d9e10da1384ce64f740b40
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2020-01-03 23:46:44 -0600

    Initial Push

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Sims3.SimIFace;
+
+namespace Gamefreak130
+{
+    public class WonderPowers
+    {
+        [Tunable]
+        public static bool kCJackB;
+    }
+}
